3af9700a84d7d9d3aae622ebcc644687fc400d5c
dedsec404 <muhammad.wahidin@students.amikom.ac.id>
2018-03-26 07:43:55 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 $secret_key = 'key123123';
 $secret_iv = 'puts';
-$baseurl = 'http://localhost:3000/';
+$baseurl = 'https://sekaistream1.herokuapp.com/';
 $DB_SETUP=array(
     "mysql:host=localhost;dbname=g",
     "root",
